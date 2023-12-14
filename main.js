5ce0a3710025054da1823832c81b50bd5315a061
アハメド ホセイン <hossain@skycom.jp>
2023-12-14 10:42:45 +0900

    file-add: aasa file added

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
-  // Flutter
-  // Function to receive messages 
-  function receiveMessageFromFlutter(message) {
-    document.getElementById('messageFromFlutter').innerText = message?.name;
-  }
-  // Function to send a message
-  function sendMessageToFlutter() {
-    var message ={data: "Hello from the web!",id:'1'};
-    var jsonString = JSON.stringify(message);
-    messageHandler.postMessage(jsonString);
-  }
+// Flutter
+// Function to receive messages 
+function receiveMessageFromFlutter(message) {
+  document.getElementById('messageFromFlutter').innerText = message?.name;
+}
+// Function to send a message
+function sendMessageToFlutter() {
+  var message = { data: "Hello from the web!", id: '1' };
+  var jsonString = JSON.stringify(message);
+  messageHandler.postMessage(jsonString);
+}
 
-  // Kotlin
-  // Function to receive messages  
-  function receiveMessageFromKotlin(message) {
-    const obj = JSON.parse(message);
-    document.getElementById('messageFromKotlin').innerText = obj?.name;
-  }
-  // Function to send a message  
-  function sendDataToAndoridKotlinApp() {
-    var message ={data: "Hello from the web!",id:'1'};
-    Android.showToast(message?.data);
-  }
+// Kotlin
+// Function to receive messages  
+function receiveMessageFromKotlin(message) {
+  const obj = JSON.parse(message);
+  document.getElementById('messageFromKotlin').innerText = obj?.name;
+}
+// Function to send a message  
+function sendDataToAndoridKotlinApp() {
+  var message = { data: "Hello from the web!", id: '1' };
+  Android.showToast(message?.data);
+}
 
-  // Maui
-  // Function to receive messages  
-  function receiveMessageFromMAUI(message) {
-    const obj = JSON.parse(message);
-    document.getElementById('messageFromMaui').innerText = obj?.name;
-  }
-  // Function to send a message  
-  function sendMessageToMaui() {
-    var message ={data: "Hello from the web!",id:'1'};
-    return message.data;
-  }
-  
-  // iOS
-  // Function to receive messages FROM APP TO web  
-  function receiveMessageFromiOS(message) {
-    const obj = JSON.parse(message);
-    document.getElementById('messageFromiOS').innerText = obj?.name;
-  }
-  // Function to send a message  from web to app
-  function sendMessageToiOS() {
-    var message ={data: "Hello from the web!",id:'1'};
-  
-     window.webkit.messageHandlers.jsMessageHandler.postMessage(message.data);
-  }
+// Maui
+// Function to receive messages  
+function receiveMessageFromMAUI(message) {
+  const obj = JSON.parse(message);
+  document.getElementById('messageFromMaui').innerText = obj?.name;
+}
+// Function to send a message  
+function sendMessageToMaui() {
+  var message = { data: "Hello from the web!", id: '1' };
+  return message.data;
+}
+
+// iOS
+// Function to receive messages FROM APP TO web  
+function receiveMessageFromiOS(message) {
+  const obj = JSON.parse(message);
+  document.getElementById('messageFromiOS').innerText = obj?.name;
+}
+// Function to send a message  from web to app
+function sendMessageToiOS() {
+  var message = { data: "Hello from the web!", id: '1' };
+
+  window.webkit.messageHandlers.jsMessageHandler.postMessage(message.data);
+}
+
+
